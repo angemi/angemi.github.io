8e1da7c41b1ad118d23081a98de887a3e98ddd9a
Krzysztof Łakwa <krzysztof.lakwa@britenet.com.pl>
2020-06-14 12:11:40 +0200

    Updates

---
+++ precache-manifest.903b2857ec903dc583d710a308a23ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a0b56b3e583b6ebdeb9b1b47686dce",
+    "url": "/grapher-web/index.html"
+  },
+  {
+    "revision": "9c249e87e66b507c819d",
+    "url": "/grapher-web/static/css/main.4982332a.chunk.css"
+  },
+  {
+    "revision": "072ceb8204ecd85310d4",
+    "url": "/grapher-web/static/js/2.4d137ef6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/grapher-web/static/js/2.4d137ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c249e87e66b507c819d",
+    "url": "/grapher-web/static/js/main.22b9542c.chunk.js"
+  },
+  {
+    "revision": "2829eecfa841eaef5e5e",
+    "url": "/grapher-web/static/js/runtime-main.cfaaf685.js"
+  }
+]);
