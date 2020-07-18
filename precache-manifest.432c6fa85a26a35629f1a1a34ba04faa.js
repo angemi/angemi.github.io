a49a0f87acf417f43561a434abf747a4d0330e59
krlakwa <krlakwa@users.noreply.github.com>
2020-07-18 09:27:19 +0000

    deploy: a72ed24974b2078aaded0318d9388254bf46053f

--- precache-manifest.432c6fa85a26a35629f1a1a34ba04faa.js
+++ precache-manifest.432c6fa85a26a35629f1a1a34ba04faa.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c24d4d6a139b62c92eb0158ecd81410",
+    "revision": "59de34250820768aa1f78d98b66ebf1f",
     "url": "/grapher-web/index.html"
   },
   {
-    "revision": "4f9109a0b008e488729c",
+    "revision": "d71c9eb977ebc5b3b20c",
     "url": "/grapher-web/static/css/main.9fba7be1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/grapher-web/static/js/2.00075e68.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f9109a0b008e488729c",
-    "url": "/grapher-web/static/js/main.843ae254.chunk.js"
+    "revision": "d71c9eb977ebc5b3b20c",
+    "url": "/grapher-web/static/js/main.7b93ff95.chunk.js"
   },
   {
     "revision": "0deaf6fa8d32bc97e280",
